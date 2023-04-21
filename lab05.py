596c4e18b18da98142f303cada30dddf01dc11fe
Sofia <129093284+sofiavvb@users.noreply.github.com>
2023-04-21 17:05:52 -0300

    inimigos neutralizados

--- lab05.py
+++ lab05.py
@@ -1,19 +1,88 @@
-genoma = input()
-x = int(input()) #por enquanto enquanto n faço as entradas: reverter
-y = int(input()) #por enquanto enquanto n faço as entradas: reverter
-a = int(input()) #por enquanto enquanto n faço as entradas: transpor
-b = int(input()) #por enquanto enquanto n faço as entradas: transpor
-c = int(input()) #por enquanto enquanto n faço as entradas: transpor
-g = input()      #por enquanto enquanto n faço as entradas: combinar
-d = int(input()) #por enquanto n faço as entradas: combinar
-genoma_informado = input() #concatenar
-w = int(input())
-z = int(input())
+def main():
+    genoma = input()
+    while True:
+        operacao = input().split()
+
+        if operacao[0] == "reverter":
+                i = int(operacao[1])
+                j = int(operacao[2])
+                if j > len(genoma):
+                    j = len(genoma)
+                if i > len(genoma):
+                    i = len(genoma)
+                genoma = reverter(genoma, i, j)
+                operacao = []
+
+        elif operacao[0] == "transpor":
+            i = int(operacao[1])
+            j = int(operacao[2])
+            k = int(operacao[3])
+            if j > len(genoma):
+                j = len(genoma)
+            if k > len(genoma):
+                k = len(genoma)
+            if i > len(genoma):
+                i = len(genoma)
+            genoma = transpor(genoma, i, j, k)
+            operacao = []
+        
+        elif operacao[0] == "combinar":
+            g = operacao[1]
+            i = int(operacao[2])
+            if i > len(genoma):
+                i = len(genoma)
+            genoma = combinar(genoma, g, i)
+            operacao = []
+        
+        elif operacao[0] == "concatenar":
+            g = operacao[1]
+            genoma = concatenar(genoma, g)
+            operacao = []
+        
+        elif operacao[0] == "remover":
+            i = int(operacao[1])
+            j = int(operacao[2])
+            if j > len(genoma):
+                j = len(genoma)
+            if i > len(genoma):
+                i = len(genoma)
+            genoma = remover(genoma, i, j)
+            operacao = []
+
+        elif operacao[0] == "transpor_e_reverter":
+            i = int(operacao[1])
+            j = int(operacao[2])
+            k = int(operacao[3])
+            if j > len(genoma):
+                j = len(genoma)
+            if k > len(genoma):
+                k = len(genoma)
+            if i > len(genoma):
+                i = len(genoma)
+            genoma = transpor_e_reverter(genoma, i, j, k)
+            operacao = []
+
+        elif operacao[0] == "buscar":
+            g = operacao[1]
+            print(buscar(genoma, g))
+            operacao = []
+
+        elif operacao[0] == "buscar_bidirecional":
+            g = operacao[1]
+            print(buscar_bidirecional(genoma, g))
+            operacao = []
+
+        elif operacao[0] == "mostrar":
+            mostrar(genoma)
+
+        elif operacao[0] == "sair":
+            break
 
 
-def reverter(genoma, i,j):
-    reversa = genoma[j:i-1:-1]
-    genoma = genoma[:i] + reversa + genoma[j+1:]
+def reverter(genoma, i, j):
+    reversa = genoma[i:j+1]
+    revertida = reversa[::-1]
+    genoma = genoma[:i] + revertida + genoma[j+1:]
     return genoma
 
 def transpor(genoma, i, j, k):
@@ -31,18 +100,24 @@ def concatenar(genoma, g):
     return genoma
 
 def remover(genoma, i, j):
-    genoma = genoma[:i] + genoma[j+1:]
+    if (i<= len(genoma)):
+        genoma = genoma[:i] + genoma[j+1:]
     return genoma
 
+def transpor_e_reverter(genoma, i, j , k):
+    genoma = transpor(genoma, i, j, k)
+    genoma = reverter(genoma, i, k)
+    return genoma
 
-reversao = reverter(genoma, x,y)
-transposta = transpor(genoma, a, b, c)
-combinacao = combinar(genoma, g, d)
-concatenacao = concatenar(genoma, genoma_informado)
-remocao = remover(genoma, w, z)
-print(reversao)
-print(transposta)
-print(combinacao)
-print(concatenacao)
-print(remocao)
- 
+def buscar(genoma, g):
+    achou = genoma.count(g)
+    return achou
+
+def buscar_bidirecional(genoma, g):
+    achou = buscar(genoma, g) + buscar(genoma[::-1], g)
+    return achou
+    
+def mostrar(genoma):
+    print(genoma)
+
+main()
