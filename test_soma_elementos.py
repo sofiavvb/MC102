7e07a48c7d41a65efcbb1a6a7ac357d77c731e89
Sofia <129093284+sofiavvb@users.noreply.github.com>
2023-04-29 21:00:27 -0300

    Delete test_soma_elementos.py

--- test_soma_elementos.py
+++
@@ -1,8 +0,0 @@
-from lab06 import soma_elementos
-
-
-def test_soma_elementos() -> None:
-    assert soma_elementos([2]) == 2
-    assert soma_elementos([1, -2, 3, 10]) == 12
-    assert soma_elementos([1, -1]) == 0
-    assert soma_elementos([]) == 0
