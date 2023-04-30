b10d0ccea4b4ccb4fa71a4716826b7612bd58bdc
Sofia <129093284+sofiavvb@users.noreply.github.com>
2023-04-29 21:00:46 -0300

    Delete test_n_duplicacao.py

--- test_n_duplicacao.py
+++
@@ -1,8 +0,0 @@
-from lab06 import n_duplicacao
-
-
-def test_n_duplicacao() -> None:
-    assert n_duplicacao([1], 4) == [1, 1, 1, 1]
-    assert n_duplicacao([1, -2], 3) == [1, -2, 1, -2, 1, -2]
-    assert n_duplicacao([1, 2], 0) == []
-    assert n_duplicacao([], 2) == []
